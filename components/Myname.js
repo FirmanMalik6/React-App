cb5e7fdc1d3f94dbe7aced4190661d978def9546
FirmanMalik6 <139314080+FirmanMalik6@users.noreply.github.com>
2023-08-07 08:01:55 +0700

    Add files via upload

---
+++ components/Myname.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+
+export const Myname = () => {
+    return <div>Firman Malik Aziz
+        <div class="p-2 g-col-6"></div>
+        <Link to="/"> <button type="button" class="btn btn-outline-info btn-lg">Home</button>
+        </Link>
+    </div>
+}
